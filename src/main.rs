7baa8715983537d0177e3ad3445d0fb714f95fa7
Dominik Schneider <dominik.p.s@outlook.de>
2023-01-02 03:59:53 +0100

    implemented MediaType enumerator with basic types and conversion

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,42 @@ use std::{
     net::{TcpListener, TcpStream},
 };
 
+enum MediaType {
+    PLAIN,
+    HTML,
+    CSS,
+    JAVASCRIPT,
+    JSON,
+    PNG,
+    JPEG
+}
+
+impl MediaType {
+    fn parse(extension: &str) -> MediaType {
+        match extension {
+            "htm" | "html"  => MediaType::HTML,
+            "css"           => MediaType::CSS,
+            "js"            => MediaType::JAVASCRIPT,
+            "json"          => MediaType::JSON,
+            "png"           => MediaType::PNG,
+            "jpg" | "jpeg"  => MediaType::JPEG,
+            "txt" | _       => MediaType::PLAIN
+        }
+    }
+
+    fn content_type(&self) -> &str {
+        match *self {
+            MediaType::HTML         => "text/html",
+            MediaType::CSS          => "text/css",
+            MediaType::JAVASCRIPT   => "application/css",
+            MediaType::JSON         => "application/json",
+            MediaType::PNG          => "image/png",
+            MediaType::JPEG         => "image/jped",
+            MediaType::PLAIN        => "text/plain",
+        }
+    }
+}
+
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
 
@@ -21,5 +57,10 @@ fn main() {
             .collect();
     
         println!("Request: {:#?}", http_request);
+
+        let first_line: Vec<&str> = http_request[0].split(' ').collect();
+        let extension = first_line[1].split('.').last().unwrap();
+        let media: MediaType = MediaType::parse(&extension);
+        println!("MIME: {}", media.content_type());
     }
 }
